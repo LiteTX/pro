7a835b38bcae97d29c0de93e350c303dba781b32
Benjamin Dev <Benjamin_dev@Benjamins-MacBook-Pro.local>
2022-08-02 13:08:42 +0100

    all

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword22 = 'mongodb://localhost:27017/mern2';
+dbPassword= 'mongodb+srv://proanalysis:HelloToday12@cluster0.5dmogmo.mongodb.net/test';
+module.exports = {
+    mongoURI: dbPassword
+};
